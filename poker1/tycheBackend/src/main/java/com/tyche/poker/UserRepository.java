17c265a096ae218d4f0ada43d6643368b6f6024e
JamesRSmith <jamesrichardsmith97@gmail.com>
2020-04-18 18:09:06 +0100

    had to change tactics - poker1

---
+++ poker1/tycheBackend/src/main/java/com/tyche/poker/UserRepository.java
@@ -0,0 +1,14 @@
+package com.tyche.poker;
+
+import com.tyche.poker.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUuid(String uuid);
+
+
+    User findByName(String name);
+
+
+}
